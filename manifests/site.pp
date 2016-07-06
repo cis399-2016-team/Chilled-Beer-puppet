56d4f36fd0064e035c6d3b42b8f118f989b82f5b
chendersUofO <chenders@uoregon.edu>
2016-07-06 16:41:07 -0700

    I added code to the site.pp file.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,10 @@
 node ip-10-0-1-25 {
+	cron cron { "puppet update":
+	    command => "cd /etc/puppet && git pull -q origin master",
+	    user    => root,
+	    minute  => "*/5",
+	}
+
     }
 
     node ip-10-0-1-24 {
