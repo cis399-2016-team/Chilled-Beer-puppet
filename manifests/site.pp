f7e91270e7f820bdeb8899dd64eef5b63f03a68b
chendersUofO <chenders@uoregon.edu>
2016-07-11 15:03:32 -0700

    Update repo check to 3 min intervals

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,7 @@ node ip-10-0-1-25 {
 	cron { "puppet update":
 	    command => "cd /etc/puppet && git pull -q origin master",
 	    user    => root,
-	    minute  => "*/5",
+	    minute  => "*/3",
 	}
 
     }
