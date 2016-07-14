c77123558327f83bb1ae5f48293334035a905894
chendersUofO <chenders@uoregon.edu>
2016-07-14 12:13:07 -0700

    Found mispelling and corrected.

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -33,7 +33,7 @@ class sshd {
 		subscribe  => File["/etc/ssh/manifest/ssh/ssh_config"],
 	}
 
-	ssh_authorized_key { "henders-key-pair-oregon":
+	ssh_authorized_key { "chenders-key-pair-oregon":
     	user => "ubuntu",
 		key  => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCWR1HbA7kQtEHE2qHTQnuTNsEGQtz8cMg5HID4WaMwOdNKGYR4ARmXHhI6Lpn2/kQ97ay2j1M1tYZhJSsglqrsGRY+gxEyr3Yp5Aq4KVLqYEnvHV5cfRbpekD3zGULH2dri8C/c/Z8ASBNHmm3deLz1IiB2IetTZ3fCS3VAqvlEF6WIbXw13j5kGGHbPG/TFevdKXwQTAK75P1LaNx00vjTHPP03/nmwRItl3lLuYS8RXyrOtmJR/oxCWqu+WZCBV5Dz37idScZ/kwrbjaDNTBrydC5lArn2x6m2r9JIn5cjI7hf3FZAfOkXSivxBTlpZ3hYW41XvHewLo3kKVIamX chenders-key-pair-oregon"
 	}
