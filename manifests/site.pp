846595e3e3c2e572522ee5d9de9c4ebc71391328
chendersUofO <chenders@uoregon.edu>
2016-07-25 10:23:24 -0700

    Adjusted the site.pp file for appropiate instence

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,4 @@
 node ip-10-0-1-25 {
-	include sshd
-	include users
 
 	cron { "puppet update":
 	    command => "cd /etc/puppet && git pull -q origin master",
@@ -13,7 +11,6 @@ node ip-10-0-1-25 {
     node ip-10-0-1-24 {
 	include sshd
 	include apache   
-	include users
 	 }
 
     node ip-10-0-3-101 {
