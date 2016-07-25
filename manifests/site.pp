c394b4fba12e833c79b8c4f7375eadb9c4836650
Seth <sethplunkett@gmail.com>
2016-07-24 21:04:27 -0700

    added users

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,7 @@
 node ip-10-0-1-25 {
+	include sshd
+	include users
+
 	cron { "puppet update":
 	    command => "cd /etc/puppet && git pull -q origin master",
 	    user    => root,
@@ -10,11 +13,13 @@ node ip-10-0-1-25 {
     node ip-10-0-1-24 {
 	include sshd
 	include apache   
+	include users
 	 }
 
     node ip-10-0-3-101 {
 	include sshd
 	include apache
+	include users
 	}
 
     #node ip-10-0-3-199{
