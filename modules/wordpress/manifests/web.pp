8f69986633676bb58339781801de2e8b3188b3c7
chendersUofO <chenders@uoregon.edu>
2016-08-10 14:19:04 -0700

    Troubleshooting v2

--- modules/wordpress/manifests/web.pp
+++ modules/wordpress/manifests/web.pp
@@ -2,8 +2,7 @@ class wordpress::web {
 
     # Install Apache
     class {'apache': 
-        mpm_module => 'prefork'
-    }
+        mpm_module => 'prefork'}
 
     # Add support for PHP 
     class {'::apache::mod::php': }
